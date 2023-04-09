8ff85cba8e30b720838dc74770fb791da34650ed
Jelena Popovic <popovicccjelena997@gmail.comm>
2023-04-09 12:24:49 +0200

    hello

--- src/components/redux/reduxthunk.js
+++ src/components/redux/reduxthunk.js
@@ -2,54 +2,54 @@ import axios from 'axios'
 import { createAsyncThunk} from '@reduxjs/toolkit'
 
 const getElement=createAsyncThunk("/getelement",async()=>{
-   const response= await axios.get("http://localhost:5000/shop")
+   const response= await axios.get("https://webshopapp.herokuapp.com/shop")
    return response.data
 }
 )
 const getUsers=createAsyncThunk("/getusers",async()=>{
-   const response= await axios.get("http://localhost:5000/shop/getusers")
+   const response= await axios.get("https://webshopapp.herokuapp.com/shop/getusers")
    return response.data
 }
 )
 const AddUser=createAsyncThunk("/adduser",async(item)=>{
-   const response= await axios.post("http://localhost:5000/shop/users", item)
+   const response= await axios.post("https://webshopapp.herokuapp.com/shop/users", item)
     return response.data
  })
  const loginuser=createAsyncThunk("/loginuser",async(item)=>{
-   const response= await axios.post(`http://localhost:5000/shop/login`, item)
+   const response= await axios.post(`https://webshopapp.herokuapp.com/shop/login`, item)
     return response
  })
  const addComment=createAsyncThunk("/addcomment",async(item)=>{
-    await axios.put(`http://localhost:5000/shop/comment/query?email=${item.email}&_id=${item._id}`, item)
+    await axios.put(`https://webshopapp.herokuapp.com/shop/comment/query?email=${item.email}&_id=${item._id}`, item)
     return item
  })
  const deletecomment=createAsyncThunk("/deletecomment",async(item)=>{
-   await axios.put(`http://localhost:5000/shop/deletecomment/query?_id=${item._id}&productId=${item.productId}`, item)
+   await axios.put(`https://webshopapp.herokuapp.com/shop/deletecomment/query?_id=${item._id}&productId=${item.productId}`, item)
    return item
 })
 
  const like=createAsyncThunk("/like",async(item)=>{
-    await axios.put(`http://localhost:5000/shop/likepost/query?email=${item.email}&_id=${item._id}`, item)
+    await axios.put(`https://webshopapp.herokuapp.com/shop/likepost/query?email=${item.email}&_id=${item._id}`, item)
     return item
  })
  const unlike=createAsyncThunk("/unlike",async(item)=>{
-    await axios.put(`http://localhost:5000/shop/unlikepost/query?email=${item.email}&_id=${item._id}`,item)
+    await axios.put(`https://webshopapp.herokuapp.com/shop/unlikepost/query?email=${item.email}&_id=${item._id}`,item)
     return item
  })
  const addNote=createAsyncThunk("/note",async(item)=>{
-  await axios.put(`http://localhost:5000/shop/notepost/query?email=${item.email}&_id=${item._id}`, item)
+  await axios.put(`https://webshopapp.herokuapp.com/shop/notepost/query?email=${item.email}&_id=${item._id}`, item)
    return item
 })
  const addsale=createAsyncThunk("/addsale",async(item,email)=>{
-    await axios.put(`http://localhost:5000/shop/query?email=${email}&_id=${item._id}/addsale`, item)
+    await axios.put(`https://webshopapp.herokuapp.com/shop/query?email=${email}&_id=${item._id}/addsale`, item)
     return {item,email}
  })
  const addPorduct=createAsyncThunk("/addproduct",async(item)=>{
-    await axios.post(`http://localhost:5000/shop/addproduct`, item)
+    await axios.post(`https://webshopapp.herokuapp.com/shop/addproduct`, item)
     return item
  })
  const byeitem=createAsyncThunk("/bye",async(item)=>{
-  const response= await axios.post(`http://localhost:5000/shop/bye`, item)
+  const response= await axios.post(`https://webshopapp.herokuapp.com/shop/bye`, item)
    return response.data
 })
 
